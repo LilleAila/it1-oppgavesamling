6924ccd935e31548f49ad752e925788a78e6ea0d
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-10-28 13:08:36 +0100

    Lagt til eks. med switch

--- oppgaver/løsningsforslag/ifelse-førerkort/førerkort-enkel.js
+++ oppgaver/løsningsforslag/ifelse-førerkort/førerkort-enkel.js
@@ -22,3 +22,25 @@ else {
 }
 
 // NB: Lag gjerne et flytskjema for mer komplekse if-else-strukturer!
+
+// Switch egner seg egentlig ikke så godt her, men vi kan bruke det med litt kreativitet:
+// switch (true) {
+//     case alder < 16:
+//         console.log("Ha deg vekk!");
+//         break;
+//     case alder < 18:
+//         console.log("Moped");
+//         break;
+//     case alder < 21:
+//         console.log("Moped og bil");
+//         break;
+//     case alder < 24:
+//         console.log("Moped, bil og lastebil");
+//         break;
+//     case alder >= 24 && alder < 120:
+//         console.log("Moped, bil, lastebil og buss.");
+//         break;
+//     default:
+//         console.log("Ugyldig alder.");
+//         break;
+// }
