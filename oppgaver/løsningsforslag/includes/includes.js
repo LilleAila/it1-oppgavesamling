d6fea83c29c48e54d0c68911a5b5ad3cb3d554f0
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-11-06 10:23:16 +0100

    Lagt til versjon som sjekkar om negasjon er brukt

--- oppgaver/løsningsforslag/includes/includes.js
+++ oppgaver/løsningsforslag/includes/includes.js
@@ -16,3 +16,26 @@ function lesMelding(evt) {
         utdata.innerText = "Me er ikkje kompatible.";
     }
 }
+
+// Avansert versjon som sjekkar for "negasjons-sjekk", altså at du seier at du IKKJE likar noko
+
+// function lesMelding(evt) {
+//     evt.preventDefault(); // SPA (single page application) = går ikkje til side 2 for å håndtere det
+
+//     let setning = document.getElementById("chatInn").value.toLowerCase();
+//     console.log(setning);
+
+//     const ord = "chips";
+
+//     // Finn ordet
+//     const harOrd = new RegExp(`\\b${ord}\\b`, "i").test(setning);
+
+//     // Sjekk mønster: "ikkje ... ord" (opp til 2 ord mellom)
+//     const negasjonFør = new RegExp(`\\bikkje\\b(?:\\s+\\w+){0,2}\\s+\\b${ord}\\b`, "i").test(setning);
+
+//     if (harOrd && !negasjonFør) {
+//         console.log(`Å, eg òg likar ${ord}!`);
+//     } else {
+//         console.log(`Då kan ikkje me snakke saman lenger, ${ord} er veldig viktig for meg!`);
+//     }
+// }
